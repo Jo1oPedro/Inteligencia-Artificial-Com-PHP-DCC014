913b6705b894d9737385ca0d16c1ebb14bbbabd9
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-09-27 14:22:24 -0300

    adiciona classe main para o algoritmo de backTracking

---
+++ backTracking/backTracking.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once "autoload.php";
+
+use src\Tree;
+use src\Node;
+
+$arvore = new Tree();
+$node = new Node();
+
+//$arvore->dale();
+$matrix = new SplFixedArray(3);
+foreach($matrix as $key => $item) {
+    $matrix[$key] = new SplFixedArray(3);
+    foreach($matrix[$key] as $key2 => $element) {
+        $matrix[$key][$key2] = $key+1 * $key2;
+    }
+}
+
+$node->setMatrix($matrix);
+
+$node2 = new Node();
+
+foreach($matrix as $key => $item) {
+    $matrix[$key] = new SplFixedArray(3);
+    foreach($matrix[$key] as $key2 => $element) {
+        $matrix[$key][$key2] = (($key+1)*3) * $key2;
+    }
+}
+
+$node2->setMatrix($matrix);
