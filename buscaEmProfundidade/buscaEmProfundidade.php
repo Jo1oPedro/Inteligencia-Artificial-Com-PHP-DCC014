01f8b93d1e32dd6e3fc13f399a4d9ed7b0dc4e50
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-10-03 02:11:19 -0300

    adiciona estrutura inicial da busca em profundidade

---
+++ buscaEmProfundidade/buscaEmProfundidade.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "autoload.php";
+
+use src\Tree;
+
+$start_time = microtime(true);
+
+$arvore = new Tree();
+
+$arvore->buscaEmProfundidade();
+
+$end_time = microtime(true);
+
+echo $execution_time = ($end_time - $start_time);
+
---
+++ buscaEmProfundidade/src/Node.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace src;
+
+use SplFixedArray;
+
+class Node {
+
+    // "?" means that it can be null
+    private ?Node $previous = null;
+    private ?Node $next = null;
+    private mixed $matrix;
+    private int $rule = 1;
+    private int $numberToInsert = 1;
+    private mixed $sons;
+    private int $numberOfSons = 0;
+    private bool $magicSquareCompleted = false;
+    private bool $invalidNode = false;
+
+    public function __construct()
+    {
+        $this->matrix = new SplFixedArray(3);
+        foreach($this->matrix as $key => $array) {
+            $this->matrix[$key] = new SplFixedArray(3);
+        }
+        $this->sons = new SplFixedArray(9);
+    }
+
+    /**
+     * Define o nó anterior
+     * @param Node $node
+     */
+    public function setPrevious(Node $node): void 
+    {
+        $this->previous = $node;
+    }
+
+    /**
+     * Retorna o nó anterior
+     */
+    public function getPrevious(): ?Node 
+    {
+        return $this->previous;
+    }
+
+    /**
+     * Define o proximo nó
+     * @param null|Node $node
+     */
+    public function setNext(Node $node = null): void 
+    {
+        $this->next = $node;
+    }
+
+    /**
+     * Retorna o proximo nó
+     */
+    public function getNext(): ?Node
+    {
+        return $this->next;
+    }
+
+    /**
+     * Constrói a matriz baseada na matriz do nó anterior;
+     * @param mixed $matrix
+     */
+    public function setMatrix(mixed $matrix): void 
+    {
+        foreach($matrix as $key => $array) {
+            foreach($array as $key2 => $element) {
+                $this->matrix[$key][$key2] = $element;
+            }
+        }
+    }
+
+    /**
+     * Retorna a matriz do nó
+     */
+    public function getMatrix(): mixed
+    {
+        return $this->matrix;
+    }
+
+    /**
+     * Define a regra atual do nó
+     * @param int $rule
+     */
+    public function setRule(int $rule): void
+    {
+        $this->rule = $rule;
+    }
+
+    /**
+     * Retorna a regra atual do nó
+     */
+    public function getRule(): int 
+    {
+        return $this->rule;
+    }
+
+    /**
+     * Seta o número que o nó atual irá inserir
+     * @param int $numberToInsert
+     */
+    public function setNumberToInsert(int $numberToInsert): void 
+    {
+        $this->numberToInsert = $numberToInsert;
+    }
+
+    /**
+     * Recupera o número que o nó atual irá inserir
+     */
+    public function getNumberToInsert(): int 
+    {
+        return $this->numberToInsert;
+    }
+
+    /**
+     * Seta os filhos do nó pai
+     * @param Node $nodeSon
+     */
+    public function setSon(Node $nodeSon): void 
+    {
+        $this->sons[$this->numberOfSons] = $nodeSon;
+        $this->numberOfSons++;
+    } 
+
+    /**
+     * Retorna o filho especificado do nó atual
+     * @param int $position
+     */
+    public function getSon(int $position): ?Node
+    {
+        return $this->sons[$position];
+    }
+
+    /**
+     * Retorna o número de filhos atuais no nó
+     */
+    public function getNumberOfSons(): int
+    {
+        return $this->numberOfSons;
+    }
+    
+    /**
+     * Seta o quadrado mágico como completo
+     */
+    public function setMagicSquareCompleted(): void 
+    {
+        $this->magicSquareCompleted = true;
+    }
+
+    /**
+     * Retorna se o quadrado mágico está completo
+     */
+    public function getMagicSquareCompleted(): bool 
+    {
+        return $this->magicSquareCompleted;
+    }
+
+    /**
+     * Seta o node como invalido.
+     */
+    public function setInvalidNode(): void 
+    {   
+        $this->invalidNode = true;
+    }
+
+    public function getInvalidNode(): bool
+    {
+        return $this->invalidNode;
+    }
+
+
+}
