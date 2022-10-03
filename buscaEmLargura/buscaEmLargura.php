73ba0339cddb18456f3a801e4bc512fc56a3e893
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-10-03 02:09:09 -0300

    retira comentarios da busca em largura

--- buscaEmLargura/buscaEmLargura.php
+++ buscaEmLargura/buscaEmLargura.php
@@ -3,16 +3,6 @@
 require_once "autoload.php";
 
 use src\Tree;
-use src\Node;
-
-/*$node = new Node();
-$node2 = new Node();
-$node->setNext($node2);
-$node->getNext()->getMatrix()[0][0] = 19;
-$node2->getMatrix()[2][2] = 47;
-print_r($node2->getMatrix());
-print_r($node->getNext()->getMatrix());
-die();*/
 
 $start_time = microtime(true);
 
